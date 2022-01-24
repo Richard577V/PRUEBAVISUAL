826ad5f6d9fb05f1b41d5524cb83332a943604fd
Richard Altamirano <61747656+Richard577V@users.noreply.github.com>
2022-01-24 18:46:16 -0500

    Prueba

---
+++ Prueba/Modelo/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Address
+    {
+        //Atributos 
+        public int address_id { get; set; }
+        public string address { get; set; }
+        public string address2 { get; set; }
+        public string district { get; set; }
+        public int city_id { get; set; }
+        public float postal_code { get; set; }
+        public float phone { get; set; }
+        public string last_update { get; set; }
+    }
+}
---
+++ Prueba/Modelo/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class City
+    {
+        //Atributos 
+        public int city_id { get; set; }
+        public string city { get; set; }
+        public int country_id { get; set; }
+        public string last_update { get; set; }
+    }
+}
